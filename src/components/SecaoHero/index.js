98264c050493ce532c6c4afb29f03de5b99fd4df
Érick Fernandes <erickfernandess2@gmail.com>
2023-09-25 16:01:17 -0500

    Primeiro commit

---
+++ src/components/SecaoHero/index.js
@@ -0,0 +1,21 @@
+export default function SecaoHero(){
+    return(
+        <section>
+            <header>
+            <img />
+
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Sobre nós</a></li>
+                <li><a href='#'>Serviços</a></li>
+                <li><a href='#'>Serviços</a></li>
+            </ul>
+
+            <button>Fale concosco</button>
+            </header>
+
+            <h2>Cuidando do seu negócio e do Meio Ambiente</h2>
+            <p>Conheça o nosso portifólio de serviços para apoiar empresas em suas iniciativas de sustentabilidade.</p>
+        </section>
+    )
+} 
