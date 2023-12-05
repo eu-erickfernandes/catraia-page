9b7963848bf1f69fe86b9e40ecfb373d7a8b0101
Erick <erickfernandess2@gmail.com>
2023-12-05 14:51:36 -0500

    Corrigindo ortografia

--- app/secoes/Sobre/index.jsx
+++ app/secoes/Sobre/index.jsx
@@ -13,10 +13,10 @@ const Sobre = () => {
                 <h2 className={ `secao__titulo ${ styles.sobre__titulo }` }>Um pouco da nossa história</h2>
 
                 <img className={ styles.sobre__icone } src='./icones-sobre/clock.svg' alt='Ícone' loading='lazy'/>
-                <p className={ styles.sobre__paragrafo }>Desde 2020, vabilizamos atividades voltadas ao desenvolvimento socioeconômico, ambiental e gestão sustentável dos recursos naturais.</p>
+                <p className={ styles.sobre__paragrafo }>Desde 2020 viabilizamos atividades voltadas ao desenvolvimento socioeconômico, ambiental e gestão sustentável dos recursos naturais.</p>
 
                 <img className={ styles.sobre__icone } src='./icones-sobre/compass.svg' alt='Ícone' loading='lazy'/>
-                <p className={ styles.sobre__paragrafo }>Realizamos projetos com foco em sociobiodiversidade, estudos de licenciamento ambiental, inventário florestal, monitoramento e levantamento da fauna, educação ambiental, treinamentos, capacitações e oficinas participativas, além de relatórios e produtos técnicos ambientais.</p>
+                <p className={ styles.sobre__paragrafo }>Realizamos projetos com foco em estudos socioambientais, licenciamento ambiental, inventário florestal, monitoramento e levantamento da fauna, educação ambiental, treinamentos, capacitações e oficinas participativas, além de relatórios e produtos técnicos ambientais.</p>
 
                 <img className={ styles.sobre__icone } src='./icones-sobre/hands.svg' alt='Ícone' loading='lazy'/>
                 <p className={ styles.sobre__paragrafo }>Movidos pela ética e compromisso no desempenho das nossas funções, priorizamos o respeito à vida em quaisquer circustâncias.</p>
